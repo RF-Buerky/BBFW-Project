5f38e89a8973402b4cfe322979f8d10c6c3a8d8a
Daniel Baumgarten <d.baumgarten@live.de>
2017-08-03 15:57:14 +0200

    Korrektur von .gitignore; Umbenennung vom Test; pom.xml verschönert

--- src/test/kotlin/HelloWorldTest.kt
+++ src/test/kotlin/HelloWorldTest.kt
@@ -1,10 +1,11 @@
+/**
+ * Created by dbaum on 02.08.2017.
+ */
 import org.junit.Test
 
 class HelloWorldTest {
-
     @Test
-
-    fun testIt(){
+    fun testHelloWorld(){
         println("Hello, World!")
     }
 }
