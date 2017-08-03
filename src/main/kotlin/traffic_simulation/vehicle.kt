f154f83db4834d9b762fbbe0d07fd20209e9065a
Jan Wustmann <th.tutorium@gmail.com>
2017-08-03 22:25:31 +0200

    created class vehicle and defined attributes

---
+++ src/main/kotlin/traffic_simulation/vehicle.kt
@@ -0,0 +1,9 @@
+package traffic_simulation
+
+class vehicle(val ID :Int, 
+              var wannaDrive : Boolean, 
+              val capacityFactor :Double, 
+              var delayed :Boolean){
+    //Methods to be added here soon
+ 
+}
