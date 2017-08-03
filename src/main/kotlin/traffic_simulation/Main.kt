1335dfa10054a106591477608f66349ab11d60f5
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-03 16:41:20 +0200

    Parsing des Inputs geschrieben

--- src/main/kotlin/traffic_simulation/Main.kt
+++ src/main/kotlin/traffic_simulation/Main.kt
@@ -7,7 +7,7 @@ import com.univocity.parsers.csv.CsvParserSettings
 fun main (args: Array<String>){
 
 
-    // The information of vehicles, their 
+    // The information of vehicles, their
     // Setup of the parsing like symbol of separation etc.
     // in this case mostly the default settings so just a few things have to be set
     val settings = CsvParserSettings()
@@ -15,5 +15,16 @@ fun main (args: Array<String>){
     settings.isHeaderExtractionEnable = true
         // this is to make the parser ignoring the first line in the csv-file
 
+    // creating a parser with the former made settings
+    val csvParser = CsvParser(settings)
 
+    // reading of the csv-file
+    val reader = FileAccess().getReader("/driveInterest.csv")
+
+    // analyze (parse) of the readed csv
+    val allRows: MutableList<Record> = csvParser.parseAllRecords(reader)
+
+    for (record in allRows){
+        println(record)
+    }
 }
