be5b5a41aeebe8097cb570b933612416135273e7
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-03 16:35:08 +0200

    Information zu Capacity Factor entfernt aus CSV

--- src/main/kotlin/traffic_simulation/Main.kt
+++ src/main/kotlin/traffic_simulation/Main.kt
@@ -7,6 +7,7 @@ import com.univocity.parsers.csv.CsvParserSettings
 fun main (args: Array<String>){
 
 
+    // The information of vehicles, their 
     // Setup of the parsing like symbol of separation etc.
     // in this case mostly the default settings so just a few things have to be set
     val settings = CsvParserSettings()
