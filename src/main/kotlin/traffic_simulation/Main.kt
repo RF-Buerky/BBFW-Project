abd2a0d3e68398a1e05ce01667bf9f09f5f597d0
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-03 17:28:12 +0200

    Parse weiter bearbeitet

--- src/main/kotlin/traffic_simulation/Main.kt
+++ src/main/kotlin/traffic_simulation/Main.kt
@@ -3,28 +3,44 @@ package traffic_simulation
 // Imports for parsing library
 import com.univocity.parsers.csv.CsvParser
 import com.univocity.parsers.csv.CsvParserSettings
+import java.io.*
 
 fun main (args: Array<String>){
 
 
-    // The information of vehicles, their
+    // The information of vehicles and their interest to drive is given in a csv-file
+    // Therefore we use a library to parse
+
     // Setup of the parsing like symbol of separation etc.
     // in this case mostly the default settings so just a few things have to be set
     val settings = CsvParserSettings()
     settings.format.setLineSeparator("\n")
-    settings.isHeaderExtractionEnable = true
+    settings.isHeaderExtractionEnabled = true
         // this is to make the parser ignoring the first line in the csv-file
 
     // creating a parser with the former made settings
     val csvParser = CsvParser(settings)
 
     // reading of the csv-file
-    val reader = FileAccess().getReader("/driveInterest.csv")
+    val reader = FileAccess().getReader("driveInterest.csv")
 
     // analyze (parse) of the readed csv
-    val allRows: MutableList<Record> = csvParser.parseAllRecords(reader)
+    val allRows: MutableList<Array<String>> = csvParser.parseAll(reader)
 
     for (record in allRows){
         println(record)
     }
+}
+
+// This class is needet for parsing
+class FileAccess {
+
+    fun getReader(path: String):Reader {
+        return InputStreamReader(this.javaClass.getResourceAsStream(path), "UTF-8")
+    }
+
+/*    fun getWriter(path: String): Writer {
+        val file = File(path)
+        return FileWriter(file)
+    }*/
 }
