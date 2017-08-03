345109245bdfe60b49f66b9fa0a4dce0282135d6
Jan Wustmann <th.tutorium@gmail.com>
2017-08-04 00:05:52 +0200

    added function to calculate the demand of road capacity

--- src/main/kotlin/traffic_simulation/roadNetwork.kt
+++ src/main/kotlin/traffic_simulation/roadNetwork.kt
@@ -1,7 +1,22 @@
 package traffic_simulation
 
 class roadNetwork(val capacity : Double){
-//Methods to be added here soon
+
+    //methods use lists of vehicles; the lists origin is not specified at the moment
+    //as the functions dont need to know the origin to work
+
+    fun calculateDemand(vehicles:MutableList<vehicle>): Double{
+        // a list of vehicles given to the function gets screened for the capacityFactor attribute
+        //and adds these Doubles to a total capacity demand for this scenario (=1 hour)
+        var capacityDemand : Double = 0.0
+
+        for (vehicle in vehicles){
+            if(vehicle.wannaDrive){
+                capacityDemand = capacityDemand + vehicle.capacityFactor
+            }
+        }
+        return capacityDemand
+    }
 
 }
 
