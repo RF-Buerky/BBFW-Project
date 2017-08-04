3729a9d9fa2ed3c861b5e815f1a285e43e10db37
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-04 15:05:40 +0200

    Parsing des Input eingefügt

--- src/main/kotlin/traffic_simulation/Main.kt
+++ src/main/kotlin/traffic_simulation/Main.kt
@@ -8,7 +8,38 @@ import java.io.*
 
 fun main(args: Array<String>) {
 
+    // The information of vehicles and their interest to drive is given in a csv-file
+    // Therefore we use a library to parse
 
+    // Setup of the parsing like symbol of separation etc.
+    // in this case mostly the default settings so just a few things have to be set
+    val settings = CsvParserSettings()
+    settings.format.setLineSeparator("\n")
+    settings.isHeaderExtractionEnabled = true
+    // this is to make the parser ignoring the first line in the csv-file
+
+    // creating a parser with the former made settings
+    val csvParser = CsvParser(settings)
+
+    // reading of the csv-file
+    val reader = FileAccess().getReader("/driveInterest.csv")
+
+    // analyze (parse) of the readed csv
+    val allRows: MutableList<Record> = csvParser.parseAllRecords(reader)
+
+    // insert the parsed information of csv-file in usable lists
+    for (record in allRows) {
+
+        val id_String : String = record.values.get(0)
+        val wannaDrive_String : String = record.values.get(1)
+
+        val id_Int : Int = id_String.toInt()
+        val wannaDrive_Boolean : Boolean = wannaDrive_String.toBoolean()
+
+        // This function is not yet implement in the class Vehicle
+        // addInputToVehicles( id = id_Int , wannaDrive = wannaDrive_Boolean )
+
+    }
 
 
     scenario(testList, testRoad)
@@ -44,7 +75,7 @@ class FileAccess {
         fun getReader(path: String):Reader {
                 return InputStreamReader(this.javaClass.getResourceAsStream(path), "UTF-8")
             }
-    
+
     /*    fun getWriter(path: String): Writer {
         val file = File(path)
         return FileWriter(file)
