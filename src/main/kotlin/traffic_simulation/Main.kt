a803c13fa522f4228dc5991fce313a6d68a73917
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-04 15:12:43 +0200

    Alte Testliste von Fahrzeugen die nun aus CSV sind aus Main gelöscht

--- src/main/kotlin/traffic_simulation/Main.kt
+++ src/main/kotlin/traffic_simulation/Main.kt
@@ -27,7 +27,7 @@ fun main(args: Array<String>) {
     // analyze (parse) of the readed csv
     val allRows: MutableList<Record> = csvParser.parseAllRecords(reader)
 
-    // insert the parsed information of csv-file in usable lists
+    // insert the parsed information of csv-file in usable lists and use them in functions
     for (record in allRows) {
 
         val id_String : String = record.values.get(0)
@@ -42,7 +42,7 @@ fun main(args: Array<String>) {
     }
 
 
-    scenario(testList, testRoad)
+   // scenario(testList, testRoad)
 }
 
 fun scenario(vehicleList: List<Vehicle>, road: RoadNetwork){
@@ -52,21 +52,7 @@ fun scenario(vehicleList: List<Vehicle>, road: RoadNetwork){
 
 val testRoad: RoadNetwork = RoadNetwork(8.0)
 
-// Creation of sufficient cars for local testing without using CSV input for now
-val BMW     : Vehicle = Vehicle(1.0)
-val AUDI    : Vehicle = Vehicle(1.0)
-val VOLVO   : Vehicle = Vehicle(1.0)
-val VW      : Vehicle = Vehicle(1.0)
-val TESLA   : Vehicle = Vehicle(1.0)
-val SUZUKI  : Vehicle = Vehicle(1.0)
-val HONDA   : Vehicle = Vehicle(1.0)
-val FERRARI : Vehicle = Vehicle(1.0)
-val PORSCHE : Vehicle = Vehicle(1.0)
-val DAIHATSU: Vehicle = Vehicle(1.0)
-val FORD    : Vehicle = Vehicle(1.0)
-val OPEL    : Vehicle = Vehicle(1.0)
 
-val testList : List<Vehicle> = listOf(BMW, AUDI, VOLVO, VW, TESLA, SUZUKI, HONDA, FERRARI, PORSCHE, DAIHATSU, FORD, OPEL)
 
 
 // This class is needet for parsing
