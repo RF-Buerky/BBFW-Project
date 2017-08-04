754cde5e98888f30b4b5025ef5bfbad82451707f
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-05 00:47:03 +0200

    RoadNetworkTest auch auf Int-Werte angepasst damit Tests klappen

--- src/test/kotlin/RoadNetworkTest.kt
+++ src/test/kotlin/RoadNetworkTest.kt
@@ -7,40 +7,40 @@ class RoadNetworkTest {
 
     @Test
     fun doesCalculateDemandWork() {
-        val road = RoadNetwork(20.0)
+        val road = RoadNetwork(20)
 
-        val BMW     : Vehicle = Vehicle(1.0)
-        val AUDI    : Vehicle = Vehicle(1.0)
-        val VOLVO   : Vehicle = Vehicle(1.0)
+        val BMW     : Vehicle = Vehicle(1, true)
+        val AUDI    : Vehicle = Vehicle(2, true)
+        val VOLVO   : Vehicle = Vehicle(3, true)
 
         val cars : List<Vehicle> = listOf(BMW,AUDI,VOLVO)
 
         val calculated = road.calculateDemand(cars)
 
-        assertEquals(3.0,calculated,0.001)
+        assertEquals(3,calculated)
     }
 
     @Test
     fun doesCheckForTrafficJamWorkForCapacityBiggerThanDemand() {
-        val road = RoadNetwork(20.0)
+        val road = RoadNetwork(20)
 
-        val trafficJam : Boolean = road.checkForTrafficJam(3.0)
+        val trafficJam : Boolean = road.checkForTrafficJam(3)
         assertEquals(false,trafficJam)
     }
 
     @Test
     fun doesCheckForTrafficJamWorkForCapacitySmallerThanDemand() {
-        val road = RoadNetwork(2.0)
+        val road = RoadNetwork(2)
 
-        val trafficJam : Boolean = road.checkForTrafficJam(3.0)
+        val trafficJam : Boolean = road.checkForTrafficJam(3)
         assertEquals(true,trafficJam)
     }
 
     @Test
     fun doesCheckForTrafficJamWorkForCapacityEqualToDemand() {
-        val road = RoadNetwork(3.0)
+        val road = RoadNetwork(3)
 
-        val trafficJam : Boolean = road.checkForTrafficJam(3.0)
+        val trafficJam : Boolean = road.checkForTrafficJam(3)
         assertEquals(false,trafficJam)
     }
 
