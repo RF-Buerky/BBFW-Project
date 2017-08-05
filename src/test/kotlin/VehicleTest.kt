687c1d0beb855bb9e81bb9407f65b1888bc787a4
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-05 21:55:33 +0200

    Neue Tests für Vehicle und alte Tests entprechen Parameterentfernung angepasst

--- src/test/kotlin/VehicleTest.kt
+++ src/test/kotlin/VehicleTest.kt
@@ -6,14 +6,58 @@ import traffic_simulation.Vehicle
 class VehicleTest {
 
     @Test
+    fun sameVehicleDoesNotChangeAllVehiclesSize () {
+
+        Vehicle(7 , true).newVehicle( )
+        Vehicle(8 , true).newVehicle( )
+        Vehicle(9 , true).newVehicle( )
+
+        var vehicle : Vehicle = Vehicle (7 , true)
+
+        var i : Int = 0
+        for (veh in vehicle.allVehicles){
+            i = i + 1
+        }
+
+        assertEquals (3 , i )
+    }
+
+    @Test
     fun sameVehicleIdIsNotNewVehicle () {
-        var vehicles : MutableList<Vehicle> = mutableListOf(Vehicle (7 , true))
+
+        Vehicle(7 , true).newVehicle()
+        Vehicle(8 , true).newVehicle()
+        Vehicle(9 , true).newVehicle()
+
+        var vehicle : Vehicle = Vehicle (7 , true)
+
+        val result : Boolean = vehicle.isNewVehicle()
+
+        for (vehicle in vehicle.allVehicles){
+            print(vehicle.id)
+            println(vehicle.wannaDrive)
+        }
+        assertEquals (false , result )
+    }
+
+    @Test
+    fun sameVehicleIsNotAddedToAllVehicles () {
+
+        Vehicle(7 , true).newVehicle()
+        Vehicle(8 , true).newVehicle()
+        Vehicle(9 , true).newVehicle()
 
         var vehicle : Vehicle = Vehicle (7 , false)
 
-        val result : Boolean = vehicle.isNewVehicle(vehicles)
+        vehicle.newVehicle()
 
-        assertEquals (false , result )
+        var i : Int = 0
+        for (vehicle in vehicle.allVehicles){
+            i = i + 1
+        }
+
+        assertEquals (3 , i)
+
     }
 
 
