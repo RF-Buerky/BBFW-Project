d6478df0c7dd37ebd6f098c6a1dc2ef1fc5d0153
Daniel Baumgarten <d.baumgarten@live.de>
2017-08-05 19:41:12 +0200

    Neue Fkt. isNewVehicle und anpassen von newVehicle

--- src/main/kotlin/traffic_simulation/Vehicle.kt
+++ src/main/kotlin/traffic_simulation/Vehicle.kt
@@ -1,10 +1,25 @@
 package traffic_simulation
 
 class Vehicle(val id : Int, val wannaDrive : Boolean, var delayed : Boolean = false){
-    //Methods to be added here soon
 
-    fun newVehicle (id : Int , wannaDrive : Boolean) : Vehicle {
-        return Vehicle(id , wannaDrive)
+    val allVehicles : MutableList<Vehicle> = mutableListOf()
+
+    fun isNewVehicle (newVehicle : Vehicle) : Boolean {
+        var isNew : Boolean = true
+        for (vehicle in allVehicles){
+            if (vehicle.id == newVehicle.id){
+                isNew = false
+            }
+        }
+        return isNew
+    }
+
+    fun newVehicle (vehicle : Vehicle): Vehicle{
+        if (isNewVehicle(vehicle)) {
+            allVehicles.add(vehicle)
+        }
+
+        return Vehicle(id, wannaDrive)
     }
 
     fun wannaDrive (): Boolean {
