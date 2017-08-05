cd046bebc9506bf401529d22e91edfdc69232210
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-05 20:52:18 +0200

    Test geschrieben für Vehicle dass gleiches Vehicle kein neues ist

--- src/test/kotlin/VehicleTest.kt
+++ src/test/kotlin/VehicleTest.kt
@@ -1,3 +1,20 @@
-/**
- * Created by Roman on 05.08.2017.
- */
+import org.junit.Test
+import org.junit.Assert.assertEquals
+import traffic_simulation.RoadNetwork
+import traffic_simulation.Vehicle
+
+class VehicleTest {
+
+    @Test
+    fun sameVehicleIdIsNotNewVehicle () {
+        var vehicles : MutableList<Vehicle> = mutableListOf(Vehicle (7 , true))
+
+        var vehicle : Vehicle = Vehicle (7 , false)
+
+        val result : Boolean = vehicle.isNewVehicle(vehicles)
+
+        assertEquals (false , result )
+    }
+
+
+}
