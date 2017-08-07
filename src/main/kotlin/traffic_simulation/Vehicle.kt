3a1bd147fab5e3a93b301d5c6d3755bcb0c8bf82
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-08 00:14:30 +0200

    Attribut wannaDrive von Vehicle zu einer Liste gemacht

--- src/main/kotlin/traffic_simulation/Vehicle.kt
+++ src/main/kotlin/traffic_simulation/Vehicle.kt
@@ -1,6 +1,6 @@
 package traffic_simulation
 
-class Vehicle(val id: Int, val wannaDrive: Boolean) {
+class Vehicle(val id: Int, val wannaDrive: MutableList<Int>) {
     var delayed: Boolean = false //no vehicle is delayed by default
 
     fun gettingDelayed() {
