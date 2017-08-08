53312907007a8d91898d06586e96f7243d203061
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-08 22:05:54 +0200

    Tests für RoadNetwork angepasst und neu geschrieben

--- src/test/kotlin/RoadNetworkTest.kt
+++ src/test/kotlin/RoadNetworkTest.kt
@@ -4,42 +4,46 @@ import traffic_simulation.RoadNetwork
 import traffic_simulation.Vehicle
 
 class RoadNetworkTest {
-/*
+
     @Test
-    fun calculateDemand_DemandOfOneVehicle_demandIsTheVehiclesDemand() {
+    fun calculateDemandAtTimestep_DemandOfOneVehicleInFirstHour_demandIsTheVehiclesDemand() {
         val road = RoadNetwork(20)
 
-        val BMW: Vehicle = Vehicle(1, mutableListOf(3, 4, 5, 8))
+        val BMW: Vehicle = Vehicle(1, mutableListOf(1, 4, 5, 8))
 
         val cars: List<Vehicle> = listOf(BMW)
 
-        val calculated = road.calculateDemandFor24Hours(cars)
+        val calculated = road.calculateDemandAtTimestep(cars , 1)
 
-        val correctMap: MutableMap<Int , Int> = mutableMapOf( Pair(1 , 0) , Pair(2 , 0) , Pair(3 , 1) , Pair(4 , 1) , Pair(5 , 1) ,
-                Pair(6 , 0) , Pair(7 , 0) , Pair(8 , 1) , Pair(9 , 0) , Pair(10 , 0) , Pair(11 , 0) , Pair(12 , 0) ,
-                Pair(13 , 0) , Pair(14 , 0) , Pair(15 , 0) , Pair(16 , 0) , Pair(17 , 0) , Pair(18 , 0) , Pair(19 , 0) ,
-                Pair(20 , 0) , Pair(21 , 0) , Pair(22 , 0) , Pair(23 , 0) , Pair(24 , 0) )
-
-        assertEquals(correctMap, calculated)
+        assertEquals(1, calculated)
     }
 
     @Test
-    fun calculateDemand_DemandOfNoneVehicle_thereIsNoDemand() {
+    fun calculateDemandAtTimestep_DemandOfOneVehicleInLastHour_demandIsTheVehiclesDemand() {
+        val road = RoadNetwork(20)
+
+        val BMW: Vehicle = Vehicle(1, mutableListOf(1, 4, 5, 8, 24))
+
+        val cars: List<Vehicle> = listOf(BMW)
+
+        val calculated = road.calculateDemandAtTimestep(cars , 24)
+
+        assertEquals(1, calculated)
+    }
+
+    @Test
+    fun calculateDemandAtTimestep_DemandOfNoneVehicle_thereIsNoDemand() {
         val road = RoadNetwork(20)
 
         val cars: List<Vehicle> = listOf()
 
-        val calculated = road.calculateDemandFor24Hours(cars)
+        val calculated = road.calculateDemandAtTimestep(cars , 5)
 
-        val correctMap: MutableMap<Int , Int> = mutableMapOf( Pair(1 , 0) , Pair(2 , 0) , Pair(3 , 0) , Pair(4 , 0) , Pair(5 , 0) ,
-                Pair(6 , 0) , Pair(7 , 0) , Pair(8 , 0) , Pair(9 , 0) , Pair(10 , 0) , Pair(11 , 0) , Pair(12 , 0) ,
-                Pair(13 , 0) , Pair(14 , 0) , Pair(15 , 0) , Pair(16 , 0) , Pair(17 , 0) , Pair(18 , 0) , Pair(19 , 0) ,
-                Pair(20 , 0) , Pair(21 , 0) , Pair(22 , 0) , Pair(23 , 0) , Pair(24 , 0) )
-        assertEquals(correctMap, calculated)
+        assertEquals(0, calculated)
     }
 
     @Test
-    fun calculateDemand_DemandOfSeveralVehicles_demandIsTheVehiclesDemand() {
+    fun calculateDemand_DemandOfSeveralVehicles_demandIsTheSummedVehiclesDemand() {
         val road = RoadNetwork(20)
 
         val BMW: Vehicle = Vehicle(1, mutableListOf(3, 4, 5, 8, 24))
@@ -48,13 +52,24 @@ class RoadNetworkTest {
 
         val cars: List<Vehicle> = listOf(BMW, AUDI, VOLVO)
 
-        val calculated = road.calculateDemandFor24Hours(cars)
+        val calculated = road.calculateDemandAtTimestep(cars , 5)
 
-        val correctMap: MutableMap<Int , Int> = mutableMapOf( Pair(1 , 0) , Pair(2 , 0) , Pair(3 , 3) , Pair(4 , 3) , Pair(5 , 3) ,
-                Pair(6 , 0) , Pair(7 , 0) , Pair(8 , 3) , Pair(9 , 1) , Pair(10 , 1) , Pair(11 , 0) , Pair(12 , 0) ,
-                Pair(13 , 0) , Pair(14 , 0) , Pair(15 , 0) , Pair(16 , 0) , Pair(17 , 0) , Pair(18 , 0) , Pair(19 , 0) ,
-                Pair(20 , 0) , Pair(21 , 0) , Pair(22 , 0) , Pair(23 , 0) , Pair(24 , 1) )
-        assertEquals(correctMap, calculated)
+        assertEquals(3, calculated)
+    }
+
+    @Test
+    fun calculateDemand_DemandOfSeveralVehiclesNotInOrder_demandIsTheSummedVehiclesDemand() {
+        val road = RoadNetwork(20)
+
+        val BMW: Vehicle = Vehicle(1, mutableListOf(5, 8, 24, 3))
+        val AUDI: Vehicle = Vehicle(2, mutableListOf(8, 3, 4, 5, 9, 10))
+        val VOLVO: Vehicle = Vehicle(3, mutableListOf(8, 4, 3, 5, 8))
+
+        val cars: List<Vehicle> = listOf(BMW, AUDI, VOLVO)
+
+        val calculated = road.calculateDemandAtTimestep(cars , 5)
+
+        assertEquals(3, calculated)
     }
 
     @Test
@@ -67,59 +82,124 @@ class RoadNetworkTest {
 
         val cars: List<Vehicle> = listOf(BMW, AUDI, VOLVO)
 
-        val calculated = road.calculateDemandFor24Hours(cars)
+        val calculated = road.calculateDemandAtTimestep(cars, 24)
 
-        val correctMap: MutableMap<Int , Int> = mutableMapOf( Pair(1 , 0) , Pair(2 , 0) , Pair(3 , 0) , Pair(4 , 0) , Pair(5 , 0) ,
-                Pair(6 , 0) , Pair(7 , 0) , Pair(8 , 0) , Pair(9 , 0) , Pair(10 , 0) , Pair(11 , 0) , Pair(12 , 0) ,
-                Pair(13 , 0) , Pair(14 , 0) , Pair(15 , 0) , Pair(16 , 0) , Pair(17 , 0) , Pair(18 , 0) , Pair(19 , 0) ,
-                Pair(20 , 0) , Pair(21 , 0) , Pair(22 , 0) , Pair(23 , 0) , Pair(24 , 0) )
-        assertEquals(correctMap, calculated)
+        assertEquals(0, calculated)
     }
 
     @Test
-    fun doesCheckForTrafficJamWorkForCapacityBiggerThanDemand() {
+    fun isTrafficJam_capacityBiggerThanDemand_noTrafficJam() {
         val road = RoadNetwork(20)
 
-        val trafficJam: Boolean = road.checkForTrafficJamInSingleHour(mutableMapOf(Pair(3, 5)), 3)
+        val trafficJam: Boolean = road.isTrafficJam(3)
         assertEquals(false, trafficJam)
     }
 
     @Test
-    fun doesCheckForTrafficJamWorkForCapacitySmallerThanDemand() {
+    fun isTrafficJam_noCapacityCemand_noTrafficJam() {
+        val road = RoadNetwork(20)
+
+        val trafficJam: Boolean = road.isTrafficJam(0)
+        assertEquals(false, trafficJam)
+    }
+
+    @Test
+    fun isTrafficJam_capacitySmallerThanDemand_trafficJamdHappens() {
         val road = RoadNetwork(2)
 
-        val trafficJam: Boolean = road.checkForTrafficJamInSingleHour(mutableMapOf(Pair(3, 5)), 3)
+        val trafficJam: Boolean = road.isTrafficJam(3)
+
         assertEquals(true, trafficJam)
     }
 
     @Test
-    fun doesCheckForTrafficJamWorkForCapacityEqualToDemand() {
+    fun isTrafficJam_capacityEqualsDemand_noTrafficJam() {
         val road = RoadNetwork(5)
 
-        val trafficJam: Boolean = road.checkForTrafficJamInSingleHour(mutableMapOf(Pair(3, 5)), 3)
+        val trafficJam: Boolean = road.isTrafficJam(5)
+
         assertEquals(false, trafficJam)
     }
 
     @Test
-    fun roadNetworkWithZeroCapacityDelaysAlwaysCars() {
+    fun isTrafficJam_capacityAndDemandZero_noTrafficJam() {
         val road = RoadNetwork(0)
 
-        val trafficJam: Boolean = road.checkForTrafficJamInSingleHour(mutableMapOf(Pair(3, 5)), 3)
-        assertEquals(true, trafficJam)
-    }
+        val trafficJam: Boolean = road.isTrafficJam(0)
 
-    @Test
-    fun noTrafficJamIfNoCapacityNeedetDespideNoCapacityThere() {
-        val road = RoadNetwork(0)
-
-        val trafficJam: Boolean = road.checkForTrafficJamInSingleHour(mutableMapOf(Pair(3, 0)), 3)
         assertEquals(false, trafficJam)
     }
 
+    @Test
+    fun calculateDemandForTimestepAndCauseTrafficJam_RoadWithoutCapacity_everyDemandDelaysVehicle() {
+        val road = RoadNetwork(0)
+        val timestep = 8
+
+        val car1: Vehicle = Vehicle(1, mutableListOf(3, 4, 5, 8, 9, 10, 11))
+        val car2: Vehicle = Vehicle(2, mutableListOf(3, 4, 5, 8, 9, 10, 11))
+        val car3: Vehicle = Vehicle(3, mutableListOf(3, 4, 5, 8, 9, 10, 11))
+        val car4: Vehicle = Vehicle(4, mutableListOf(3, 4, 5, 8, 9, 10, 11))
+        val car5: Vehicle = Vehicle(5, mutableListOf(3, 4, 5, 8))
+        val car6: Vehicle = Vehicle(6, mutableListOf(3, 4, 5, 8))
+
+        val allVehicles: List<Vehicle> = listOf(car1, car2, car3, car4, car5, car6)
+
+        road.calculateDemandForTimestepAndCauseTrafficJam(timestep , allVehicles)
+
+        for (vehicle in allVehicles){
+            val test : Boolean = vehicle.delayedInHours.contains(8)
+            assertEquals(true, test)
+        }
+    }
 
     @Test
-    fun simulateScenario_someVehiclesGettingDelayed_NumberOfDelaysIsCorrect() {
-        val road = RoadNetwork(5)
+    fun simulateScenario_vehiclesDemandTooMuchCapacityInLastTimestep_NumberOfDelaysIsCorrect() {
+        val road = RoadNetwork(2)
+
+        val car1: Vehicle = Vehicle(1, mutableListOf(24))
+        val car2: Vehicle = Vehicle(2, mutableListOf(24))
+        val car3: Vehicle = Vehicle(3, mutableListOf(24))
+        val car4: Vehicle = Vehicle(4, mutableListOf(24))
+        val car5: Vehicle = Vehicle(5, mutableListOf(24))
+        val car6: Vehicle = Vehicle(6, mutableListOf(24))
+
+        val allVehicles: List<Vehicle> = listOf(car1, car2, car3, car4, car5, car6)
+        val output : List<Vehicle> = road.simulateScenario(allVehicles)
+
+        var i: Int = 0
+
+        for (car in output) {
+            for (hour in car.delayedInHours) {
+                i = i + 1
+            }
+        }
+        assertEquals(6, i)
+    }
+
+    @Test
+    fun simulateScenario_vehiclesDemandTooMuchCapacityInOneTimestep_delaysGoThroughAllTimestepsSinceThen() {
+        val road = RoadNetwork(2)
+
+        val car1: Vehicle = Vehicle(1, mutableListOf(19))
+        val car2: Vehicle = Vehicle(2, mutableListOf(19))
+        val car3: Vehicle = Vehicle(3, mutableListOf(19))
+
+        val allVehicles: List<Vehicle> = listOf(car1, car2, car3)
+        val output : List<Vehicle> = road.simulateScenario(allVehicles)
+
+        var i: Int = 0
+
+        for (car in output) {
+            for (hour in car.delayedInHours) {
+                i = i + 1
+            }
+        }
+        assertEquals(18, i)
+    }
+
+    @Test
+    fun simulateScenario_vehiclesDemandTooMuchCapacityInSeveralTimesteps_delaysGoThroughAllTimestepsSinceThen() {
+        val road = RoadNetwork(3)
 
         val car1: Vehicle = Vehicle(1, mutableListOf(3, 4, 5, 8, 9, 10, 11))
         val car2: Vehicle = Vehicle(2, mutableListOf(3, 4, 5, 8, 9, 10, 11))
@@ -138,7 +218,7 @@ class RoadNetworkTest {
                 i = i + 1
             }
         }
-        assertEquals(24, i)
+        assertEquals(132, i)
     }
 
     @Test
@@ -190,5 +270,4 @@ class RoadNetworkTest {
         }
         assertEquals(0, i)
     }
-    */
 }
