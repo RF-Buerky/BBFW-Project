0abf55182f65225fb80dcd6fe65147e4e90e04b8
Daniel Baumgarten <d.baumgarten@live.de>
2017-08-09 01:46:12 +0200

    Gerüst für Wahrscheinlichkeiten eingefügt

--- src/main/kotlin/traffic_simulation/RoadNetwork.kt
+++ src/main/kotlin/traffic_simulation/RoadNetwork.kt
@@ -2,14 +2,28 @@ package traffic_simulation
 
 class RoadNetwork(val capacity: Int) {
 
-    fun calculateDemandForTimestepAndCauseTrafficJam(timestep :Int, vehicleList: List<Vehicle>): Int{
-        var demandAtTimestep :Int =0
+    fun calculateDemandForTimestepAndCauseTrafficJam(timestep: Int, vehicleList: List<Vehicle>): Int{
+        var demandAtTimestep: Int =0
 
         for(vehicle in vehicleList){
             if(vehicle.vehicleWantsToDriveAt(timestep)){
                 demandAtTimestep = demandAtTimestep +1
             }
         }
+
+        val probabilityOfDelay: Int
+        val percentOfCapacityUsage: Int = (demandAtTimestep / capacity) * 100
+        when(percentOfCapacityUsage){
+            in 0..49 -> probabilityOfDelay = 5
+            in 51..69 -> probabilityOfDelay = 10
+            in 70..89 -> probabilityOfDelay = 20
+            in 90..109 -> probabilityOfDelay = 30
+            in 110..124 -> probabilityOfDelay = 50
+            in 125..149 -> probabilityOfDelay = 70
+            in 150..Int.MAX_VALUE -> probabilityOfDelay = 90
+        }
+        // The Random-parts ensue.
+
         if(demandAtTimestep>capacity){
             for(vehicle in vehicleList){
                 vehicle.getDelayedAtHour(timestep)
