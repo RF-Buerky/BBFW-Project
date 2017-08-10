27ab26e53a7edf5edafd130c2dde8a4e123d824c
R_Buerky <roman.f.buerky@th-bingen.de>
2017-08-10 21:59:24 +0200

    Reformat Code

--- src/main/kotlin/traffic_simulation/RoadNetwork.kt
+++ src/main/kotlin/traffic_simulation/RoadNetwork.kt
@@ -2,46 +2,46 @@ package traffic_simulation
 
 class RoadNetwork(val capacity: Int) {
 
-    fun calculateDemandForTimestepAndCauseRandomDelays(timestep :Int, vehicleList: List<Vehicle>){
+    fun calculateDemandForTimestepAndCauseRandomDelays(timestep: Int, vehicleList: List<Vehicle>) {
 
-        val demandAtTimestep :Int = calculateDemandAtTimestep(vehicleList , timestep)
+        val demandAtTimestep: Int = calculateDemandAtTimestep(vehicleList, timestep)
 
 
-        for(vehicle in vehicleList){
-            val randomDelay : Boolean = vehicle.randomDelayByCapacityAndDemand(demandAtTimestep, capacity)
-                    if(randomDelay){
-                        delayVehiclesInTimestep(vehicleList , timestep)
-                    } else {
-                        vehiclesDrivingInTimestep(vehicleList , timestep)
-                }
+        for (vehicle in vehicleList) {
+            val randomDelay: Boolean = vehicle.randomDelayByCapacityAndDemand(demandAtTimestep, capacity)
+            if (randomDelay) {
+                delayVehiclesInTimestep(vehicleList, timestep)
+            } else {
+                vehiclesDrivingInTimestep(vehicleList, timestep)
+            }
         }
 
     }
 
-    fun calculateDemandAtTimestep (vehicleList : List<Vehicle> , timestep : Int):Int{
-        var demandAtTimestep : Int = 0
-        for(vehicle in vehicleList){
-            if(vehicle.vehicleWantsToDriveAt(timestep)){
-                demandAtTimestep = demandAtTimestep +1
+    fun calculateDemandAtTimestep(vehicleList: List<Vehicle>, timestep: Int): Int {
+        var demandAtTimestep: Int = 0
+        for (vehicle in vehicleList) {
+            if (vehicle.vehicleWantsToDriveAt(timestep)) {
+                demandAtTimestep = demandAtTimestep + 1
             }
         }
         return demandAtTimestep
     }
 
-    fun delayVehiclesInTimestep (vehicleList : List<Vehicle> , timestep : Int){
-        for(vehicle in vehicleList){
+    fun delayVehiclesInTimestep(vehicleList: List<Vehicle>, timestep: Int) {
+        for (vehicle in vehicleList) {
             vehicle.getDelayedAtHour(timestep)
         }
     }
 
-    fun vehiclesDrivingInTimestep (vehicleList: List<Vehicle>, timestep: Int){
-        for(vehicle in vehicleList){
+    fun vehiclesDrivingInTimestep(vehicleList: List<Vehicle>, timestep: Int) {
+        for (vehicle in vehicleList) {
             vehicle.driveAtHour(timestep)
         }
     }
 
     fun simulateScenario(vehicleList: List<Vehicle>): List<Vehicle> {
-        for(timestep in 1..24){
+        for (timestep in 1..24) {
             calculateDemandForTimestepAndCauseRandomDelays(timestep, vehicleList)
         }
         return vehicleList
